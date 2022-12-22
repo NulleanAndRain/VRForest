4abd466e90d954429bd75654c8729bec6db8d028
Shchinnikov <mr.shchinnikov@mail.ru>
2022-12-22 21:02:22 +0400

    fixed AnimalMovementManager main animations needed create run away function

--- Assets/Scripts/Animal movement/AnimalMovementManager.cs
+++ Assets/Scripts/Animal movement/AnimalMovementManager.cs
@@ -13,41 +13,52 @@ public class AnimalMovementManager : MonoBehaviour
 
     private NavMeshAgent _agent;
     private int _wayID;
+    private float _localSpeed;
+    private Vector3 _oldPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         StartCoroutine("SetAnim");
+        _oldPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        _localSpeed = (transform.position.magnitude - _oldPosition.magnitude) / Time.fixedDeltaTime; //запоминаем скорость;
+        _oldPosition = transform.position; //берем новую "старую" позицию
+        if (_localSpeed != 0) animator.SetBool("IsRun", true);
+        else animator.SetBool("IsRun", false);
+        Debug.Log(_localSpeed + " " + gameObject.name);
     }
 
 
     private void GoRun()
     {
-        if (animator.GetBool("IsRun"))
+        int i = Random.Range(0, wayPoints.Length-1);
+        if (i != _wayID)
         {
-            int i = Random.Range(0, wayPoints.Length-1);
-            if (i != _wayID)
+            _wayID = i;
+            var dist = Vector3.Distance(transform.position, wayPoints[_wayID].position);
+            if (dist > 0.1f)
             {
-                _wayID = i;
-                var dist = Vector3.Distance(transform.position, wayPoints[_wayID].position);
-                if (dist > 0.1f)
-                {
-                    if (hasSpecialAnim) animator.SetBool("IsSpecial", false);
-                    Debug.Log(_agent.isStopped);
-                    _agent.SetDestination(wayPoints[i].position);
-                }
+                if (hasSpecialAnim) animator.SetBool("IsSpecial", false);
+                
+                _agent.SetDestination(wayPoints[i].position);
             }
-            else animator.SetBool("IsRun", false);
         }
+        //else animator.SetBool("IsRun", false);
+        
     }
     IEnumerator SetAnim()
     {
         while (true)
         {
-            animator.SetBool("IsRun", false);
-            yield return new WaitForSeconds(timeToNextPoint/2);
-            animator.SetBool("IsRun", true);
+            //animator.SetBool("IsRun", false);
+            //yield return new WaitForSeconds(timeToNextPoint/2);
+            //animator.SetBool("IsRun", true);
+            //yield return new WaitForSeconds(0.5f);
             GoRun();
             yield return new WaitForSeconds(timeToNextPoint);
         }
